e537263936d953b1f435317eba04cee7a27a7eed
vidyasagar0405 <vidyasagar0405@gmail.com>
2024-10-14 17:39:46 +0530

    nt

--- home/model.go
+++ home/model.go
@@ -4,47 +4,60 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
+
 type Model struct {
-	keys keymaps
+	projectName textinput.Model
+	keys        keymaps
 }
 
 func InitialModel() Model {
-    return Model{
-        keys: Keys,
-    }
+
+	projectName := textinput.New()
+	projectName.Placeholder = "Project name"
+
+	return Model{
+		projectName: projectName,
+		keys:        Keys,
+	}
 }
 
 func (m Model) Init() tea.Cmd {
-    return nil
+	return textinput.Blink
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    var cmd tea.Cmd
-    cmd = nil
+	var cmd tea.Cmd
+	cmd = nil
 
-    switch msg := msg.(type) {
-    case tea.KeyMsg:
-        switch {
-        case key.Matches(msg, m.keys.quit):
-            cmd = tea.Quit
-        }
-        return m, cmd
-    }
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keys.quit):
+			cmd = tea.Quit
+		}
+		return m, cmd
+	}
 
-    return m, nil
+	return m, nil
 }
 
 func (m Model) View() string {
 
-    headerString := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#6b53de"))
+	headerString := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#6b53de"))
 
-    displayString := "\n"
+	displayString := "\n"
 
-    displayString += fmt.Sprintf("%s\n\n%s\n\n%s\n\n%s\n", headerString.Render("Project name: "),  headerString.Render("FastQC"), headerString.Render("Multiqc"), headerString.Render("BWA"))
+	displayString += fmt.Sprintf("%s%s\n\n%s\n\n%s\n\n%s\n",
+		headerString.Render("Project name: "),
+		m.projectName.View(),
+		headerString.Render("FastQC"),
+		headerString.Render("Multiqc"),
+		headerString.Render("BWA"),
+	)
 
 	return displayString
 }
-
--- styles.go
+++ styles.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/charmbracelet/lipgloss"
+import (
+	"fmt"
 
+	"github.com/charmbracelet/lipgloss"
+)
 
 func (m mainModel) tabView() string {
 	var tabViews []string
 	for i, tab := range m.tabs {
 		style := lipgloss.NewStyle().Padding(0, 4).Border(lipgloss.NormalBorder(), false, true, false, false)
-        // separator := lipgloss.NewStyle().Foreground(lipgloss.Color("#caa5f6"))
+		// separator := lipgloss.NewStyle().Foreground(lipgloss.Color("#caa5f6"))
 		if i == m.activeTab {
 			style = style.Bold(true).Foreground(lipgloss.Color("205"))
 		}
@@ -16,7 +19,20 @@ func (m mainModel) tabView() string {
 	return lipgloss.JoinHorizontal(lipgloss.Left, tabViews...)
 }
 
-
-type DefStyles struct{
-    defaultWithPadd lipgloss.Style
+type DefStyles struct {
+	defaultWithPadd lipgloss.Style
 }
+
+var (
+	defaultWithPadd     = lipgloss.NewStyle().Padding(0, 3)
+
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
